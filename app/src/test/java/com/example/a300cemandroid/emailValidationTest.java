6ca9f4a6e4287000276e04cee009a9b0f0672d63
MeanMachine <machinaaron1@gmail.com>
2019-10-19 12:21:00 +0100

    Password validation unit tests made

---
+++ app/src/test/java/com/example/a300cemandroid/emailValidationTest.java
@@ -0,0 +1,26 @@
+package com.example.a300cemandroid;
+
+import org.junit.Test;
+
+import static com.example.a300cemandroid.emailValidation.isEmail;
+import static org.junit.Assert.*;
+
+public class emailValidationTest {
+    @Test
+    public void isEmail_emptyString() {
+        String testString = "";
+        assertFalse(isEmail(testString));
+    }
+
+    @Test
+    public void isEmail_notAnEmail() {
+        String testString = "loremipsum";
+        assertFalse(isEmail(testString));
+    }
+
+    @Test
+    public void isEmail_isAnEmail() {
+        String testString = "loremipsum@lorem.com";
+        assertTrue(isEmail(testString));
+    }
+}
