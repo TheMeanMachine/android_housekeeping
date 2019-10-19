487f158a4ebaf64942ab5bc55723722d354e8d53
MeanMachine <machinaaron1@gmail.com>
2019-10-19 16:43:47 +0100

    Name validation done

---
+++ app/src/main/java/com/example/a300cemandroid/nameValidation.java
@@ -0,0 +1,17 @@
+package com.example.a300cemandroid;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class nameValidation {
+    public static boolean isFirstName( String firstName )
+    {
+        return firstName.matches( "[A-Z][a-zA-Z]*" );
+    }
+
+
+    public static boolean isLastName( String lastName )
+    {
+        return lastName.matches( "[a-zA-z]+([ '-][a-zA-Z]+)*" );
+    }
+}
