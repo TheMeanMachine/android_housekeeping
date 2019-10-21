9d74dbd694ef513c520094772c099a9255d05dad
MeanMachine <machinaaron1@gmail.com>
2019-10-21 18:33:53 +0100

    House screen layout complete - started frontend code

--- app/src/main/java/com/example/a300cemandroid/ui/houses/housesFragment.java
+++ app/src/main/java/com/example/a300cemandroid/ui/houses/housesFragment.java
@@ -1,41 +1,78 @@
 package com.example.a300cemandroid.ui.houses;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.widget.Toast;
 
 import com.example.a300cemandroid.R;
 
-public class housesFragment extends Fragment {
+public class housesFragment extends Fragment{
 
     private housesViewModel homeViewModel;
+
+    public View view;
+
+    private FloatingActionButton faBtn;
+    private Button addMemberBtn;
+    private Button locationBtn;
+    private Button deleteBtn;
+    private Spinner housesDrop;
+    private Spinner membersDrop;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(housesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_house, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_houses);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-        return root;
+
+
+        view = inflater.inflate(R.layout.fragment_house,container,false);
+
+        faBtn = (FloatingActionButton) view.findViewById(R.id.floatingActionButton);
+        addMemberBtn = (Button) view.findViewById(R.id.inviteMemberBtn);
+        locationBtn = (Button) view.findViewById(R.id.locationBtn);
+        deleteBtn = (Button) view.findViewById(R.id.deleteHouseBtn);
+        housesDrop = (Spinner) view.findViewById(R.id.housesDropdown);
+        membersDrop = (Spinner) view.findViewById(R.id.membersDrop);
+
+        Toast.makeText(view.getContext(), "Setting up stuff bby!", Toast.LENGTH_SHORT).show();
+        setListeners();
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+
+
     }
+
+    public void setListeners(){
+        faBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "floating here boi!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "yo!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
 }
