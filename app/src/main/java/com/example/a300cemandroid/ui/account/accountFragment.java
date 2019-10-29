94f99964ebd5ac1371f2d310c52cee9d741219a4
MeanMachine <machinaaron1@gmail.com>
2019-10-29 17:08:27 +0000

    Backend start
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/a300cemandroid/ui/account/accountFragment.java
+++ app/src/main/java/com/example/a300cemandroid/ui/account/accountFragment.java
@@ -1,16 +1,13 @@
 package com.example.a300cemandroid.ui.account;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.support.annotation.Nullable;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
 
 import com.example.a300cemandroid.R;
 
@@ -18,19 +15,17 @@ public class accountFragment extends Fragment {
 
     private accountViewModel accountViewModel;
 
+    private  View view;
+
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         accountViewModel =
                 ViewModelProviders.of(this).get(accountViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_account, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_account);
-        accountViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-        return root;
+        view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        return view;
 
     }
 
--- app/src/main/java/com/example/a300cemandroid/ui/account/accountViewModel.java
+++ app/src/main/java/com/example/a300cemandroid/ui/account/accountViewModel.java
@@ -1,19 +1,17 @@
 package com.example.a300cemandroid.ui.account;
 
-import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 public class accountViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
+    private MutableLiveData<String> firstName;
+    private MutableLiveData<String> lastName;
+    private MutableLiveData<String> email;
+    
     public accountViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is account fragment");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+
 }
